5333c7e40b7bae0280d92ed67ab5b39954d6acf5
Ana Raquel Campesan <anazard@outlook.com>
2017-09-15 23:26:27 -0400

    Add functions

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func sum(num1: Float, num2: Float) -> Float {
+    return num1 + num2
+}
+print(sum(num1: 2, num2: 3))
+
+func greetings(name: String) -> String {
+    return "Salut, \(name)!"
+}
+print(greetings(name: "Ana"))
